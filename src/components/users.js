7c1a0457c83beea6a7a51c6196d47c950f2bc37a
Luis Miller <101733870+luismiller2@users.noreply.github.com>
2022-05-13 10:01:25 -0400

    first commit

--- src/components/users.js
+++ src/components/users.js
@@ -1,11 +1,77 @@
 import React from "react";
 
 const User = () => {
-    return(
-        <div>
+  return (
+    <div>
+      <body>
+        <head>
+          <meta charset="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <title>Untitled</title>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css"
+          />
+          {/* <link rel="stylesheet" href="assets/css/style.css" /> */}
+        </head>
+
         <h1>Users </h1>
+        <h3>List of users:</h3>
+        <ol>
+          <li>Luis Miller</li>
+          <li>John Appleseed</li>
+          <li>Diego Miller</li>
+          <li>Miguel Jones</li>
+        </ol>
+
+        <div class="footer-basic">
+          <footer>
+            <div class="social">
+              <a href="#">
+                <i class="icon ion-social-instagram"></i>
+              </a>
+              <a href="#">
+                <i class="icon ion-social-snapchat"></i>
+              </a>
+              <a href="#">
+                <i class="icon ion-social-twitter"></i>
+              </a>
+              <a href="#">
+                <i class="icon ion-social-facebook"></i>
+              </a>
+            </div>
+            <ul class="list-inline">
+              <li class="list-inline-item">
+                <a href="#">Home</a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">Services</a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">About</a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">Terms</a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">Privacy Policy</a>
+              </li>
+            </ul>
+            <p class="copyright">BetterMe © 2022</p>
+          </footer>
         </div>
-    )
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+      </body>
+    </div>
+  );
 };
 
 export default User;
